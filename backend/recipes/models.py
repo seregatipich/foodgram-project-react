4bef00cb035a5e46fb7bd6f012f4a43ac4ec4a2d
Sergei Poluektov <seregatipich@outlook.com>
2023-05-12 09:33:52 +0200

    fixed imports

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,7 +4,6 @@ from django.core.validators import (MaxValueValidator, MinValueValidator,
                                     RegexValidator)
 from django.db import models
 from django.db.models import UniqueConstraint
-
 from users.models import User
 
 
